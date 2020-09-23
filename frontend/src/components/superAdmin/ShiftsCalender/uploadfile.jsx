91554c67180d1c51969968fcab4744ccf38e79c6
Ehsan <ehsansoomro9@gmail.com>
2020-09-23 16:54:49 +0500

    deleteMethod

--- frontend/src/components/superAdmin/ShiftsCalender/uploadfile.jsx
+++ frontend/src/components/superAdmin/ShiftsCalender/uploadfile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "antd";
+import { Button, Upload } from "antd";
 import axios from "axios";
 import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 
@@ -81,6 +81,14 @@ class uploadfile extends Component {
   //     .catch((err) => console.log("err", err));
    };
 
+   deletePreviousData = () => {
+    axios
+        .post("http://localhost:4000/api/shift/deleteEventsBetweenTwoDates/"+this.state.startDate+'/'+this.state.endDate)
+        .then((response) => {
+          console.log("response", response);
+        })
+        .catch((err) => console.log("err", err));
+   }
   render() {
     return (
       
@@ -92,15 +100,18 @@ class uploadfile extends Component {
                     onChange={this.onFileChange}
                   />
                 {this.state.file && (
-                    <Button variant="info" onClick={this.onFileUpload}>
+                    <Upload variant="info" onClick={this.onFileUpload}>
                       Update Existing Sheet
-                    </Button>
+                    </Upload>
                 )}
             
               <br />
               <br />
               <Button className="mb-3" variant="info">
                 Download File
+              </Button>
+              <Button type="primary" className="mb-3" variant="info" onClick={this.deletePreviousData}>
+                Delete
               </Button>
 
               {this.state.rows && 

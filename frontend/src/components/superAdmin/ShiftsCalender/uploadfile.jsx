62e8ba3aad51c30278ed78605ed89b28a1c07f64
Ehsan Soomro <you@example.com>
2020-11-16 13:30:52 +0500

    review

--- frontend/src/components/superAdmin/ShiftsCalender/uploadfile.jsx
+++ frontend/src/components/superAdmin/ShiftsCalender/uploadfile.jsx
@@ -443,7 +443,6 @@ class uploadfile extends Component {
                                   <th>Day</th>
                                   <th>4th</th>
                                   <th>Off</th>
-                                  <th>Request</th>
                                  </tr>
                               </thead>       
                                         { this.state.exportExcelArr.map(value => {
@@ -460,7 +459,6 @@ class uploadfile extends Component {
                                               <td>{value.Day}</td>
                                               <td>{value['4th']}</td>
                                               <td>{value.Off}</td>
-                                              <td>{value.Request}</td>
                                           </tr>
                                             )
                                         })}

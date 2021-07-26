6ba3350bf8076b3d15e465f595315551c124bc79
Hassan Ahmed <hassanahmedleo786@gmail.com>
2021-07-25 23:42:14 -0700

    edit by admin issue resolved

--- backend/routes/shift.js
+++ backend/routes/shift.js
@@ -1230,7 +1230,7 @@ router.get('/currentShifts', (req, res) => {
 								offApprovalStatus: shift.offApprovalStatus,
 							};
 						}
-					} else if(shift.shiftTypeId) {
+					} else if(shift && shift.shiftTypeId) {
 						return {
 							_id: shift._id,
 							shiftid:shift.shiftTypeId._id,
